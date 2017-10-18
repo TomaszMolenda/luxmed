12437738efcd354035f3635824038c53009260d3
tomasz.molenda <tomasz.molenda@gmail.com>
2017-10-18 09:37:48 +0200

    init

---
+++ src/main/java/pl/tomo/luxmed/Login.java
@@ -0,0 +1,9 @@
+package pl.tomo.luxmed;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Login {
+
+
+}
---
+++ src/main/java/pl/tomo/luxmed/LuxmedApplication.java
@@ -0,0 +1,12 @@
+package pl.tomo.luxmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxmedApplication.class, args);
+	}
+}
